3d25719e1ec6137d76e1b5921a63a12289196109
kaiquemsa <kaiquemeira@hotmail.com>
2025-10-08 23:29:42 -0300

    change env

--- tcc_frontend/src/app/environments/environment.ts
+++ tcc_frontend/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiKey: 'devKey',
-  URL: 'http://localhost:8000'
+  // URL: 'http://localhost:8000'
+  URL: 'https://tcc2025-production-81ec.up.railway.app'
 };
